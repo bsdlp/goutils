562d717c2c6deef7f5f4aa2a2b0cdc9e2df75af6
Jon Chen <bsd@voltaire.sh>
2013-12-17 16:55:16 -0500

    make this less crazy

--- tail/tail.go
+++ tail/tail.go
@@ -12,21 +12,8 @@
 //              output the last K lines, instead of the last 10; or use -n +K to
 //              output lines starting with the Kth
 //
-//       --max-unchanged-stats=N
-//              with  --follow=name,  reopen  a  FILE which has not changed size
-//              after N (default 5) iterations to see if it has been unlinked or
-//              renamed  (this  is  the  usual case of rotated log files).  With
-//              inotify, this option is rarely useful.
-//
-//       --pid=PID
-//              with -f, terminate after process ID, PID dies
-//
 //       -q, --quiet, --silent
 //              never output headers giving file names
-//
-//       --retry
-//              keep trying to open a file even when it is or becomes inaccessi‐
-//              ble; useful when following by name, i.e., with --follow=name
 //
 //       -s, --sleep-interval=N
 //              with -f, sleep for approximately N seconds (default 1.0) between
