cc391949f5fb0e2eaf5feebdd43d5809a18c9f0d
Jon Chen <bsd@voltaire.sh>
2016-08-21 00:01:12 -0700

    update documentation

--- at/at.go
+++ at/at.go
@@ -10,7 +10,7 @@ import (
 // Schedule
 var ErrTimeAlreadyPassed = errors.New("at: can't schedule in the past")
 
-// Schedule queues a job to run at the specific time
+// Schedule queues a job to run within 1 second after the specified time
 func Schedule(ctx context.Context, ts time.Time, fn func(ctx context.Context)) error {
 	duration := ts.Sub(time.Now())
 	if int64(duration) < 0 {
